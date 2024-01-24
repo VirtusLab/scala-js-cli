3bf1f97ea4347cd2912900e8acbe635eaf971f3f
Simon Parten <quafadas@gmail.com>
2024-01-24 22:07:41 +0100

    add back previuos tests

--- tests/test/src/org/scalajs/cli/tests/Tests.scala
+++ tests/test/src/org/scalajs/cli/tests/Tests.scala
@@ -27,202 +27,202 @@ class Tests extends munit.FunSuite {
   def getScalaJsCompilerPlugin(cwd: os.Path) = os.proc("cs", "fetch", "--intransitive", s"org.scala-js:scalajs-compiler_2.13.12:$scalaJsVersion")
     .call(cwd = cwd).out.trim()
 
-  // test("tests") {
-  //   val dir = os.temp.dir()
-  //   os.write(
-  //     dir / "foo.scala",
-  //     """object Foo {
-  //       |  def main(args: Array[String]): Unit = {
-  //       |    println(s"asdf ${1 + 1}")
-  //       |    new A
-  //       |  }
-  //       |
-  //       |  class A
-  //       |}
-  //       |""".stripMargin
-  //   )
+  test("tests") {
+    val dir = os.temp.dir()
+    os.write(
+      dir / "foo.scala",
+      """object Foo {
+        |  def main(args: Array[String]): Unit = {
+        |    println(s"asdf ${1 + 1}")
+        |    new A
+        |  }
+        |
+        |  class A
+        |}
+        |""".stripMargin
+    )
 
-  //   val scalaJsLibraryCp = getScalaJsLibraryCp(dir)
+    val scalaJsLibraryCp = getScalaJsLibraryCp(dir)
 
-  //   os.makeDir.all(dir / "bin")
-  //   os.proc(
-  //     "cs",
-  //     "launch",
-  //     "scalac:2.13.12",
-  //     "--",
-  //     "-classpath",
-  //     scalaJsLibraryCp,
-  //     s"-Xplugin:${getScalaJsCompilerPlugin(dir)}",
-  //     "-d",
-  //     "bin",
-  //     "foo.scala"
-  //   ).call(cwd = dir, stdin = os.Inherit, stdout = os.Inherit)
+    os.makeDir.all(dir / "bin")
+    os.proc(
+      "cs",
+      "launch",
+      "scalac:2.13.12",
+      "--",
+      "-classpath",
+      scalaJsLibraryCp,
+      s"-Xplugin:${getScalaJsCompilerPlugin(dir)}",
+      "-d",
+      "bin",
+      "foo.scala"
+    ).call(cwd = dir, stdin = os.Inherit, stdout = os.Inherit)
 
-  //   val res = os
-  //     .proc(
-  //       launcher,
-  //       "--stdlib",
-  //       scalaJsLibraryCp,
-  //       "-s",
-  //       "-o",
-  //       "test.js",
-  //       "-mm",
-  //       "Foo.main",
-  //       "bin"
-  //     )
-  //     .call(cwd = dir, stderr = os.Pipe)
-  //   val expectedInOutput =
-  //     "Warning: using a single file as output (--output) is deprecated since Scala.js 1.3.0. Use --outputDir instead."
-  //   assert(res.err.text().contains(expectedInOutput))
+    val res = os
+      .proc(
+        launcher,
+        "--stdlib",
+        scalaJsLibraryCp,
+        "-s",
+        "-o",
+        "test.js",
+        "-mm",
+        "Foo.main",
+        "bin"
+      )
+      .call(cwd = dir, stderr = os.Pipe)
+    val expectedInOutput =
+      "Warning: using a single file as output (--output) is deprecated since Scala.js 1.3.0. Use --outputDir instead."
+    assert(res.err.text().contains(expectedInOutput))
 
-  //   val testJsSize = os.size(dir / "test.js")
-  //   val testJsMapSize = os.size(dir / "test.js.map")
-  //   assert(testJsSize > 0)
-  //   assert(testJsMapSize > 0)
+    val testJsSize = os.size(dir / "test.js")
+    val testJsMapSize = os.size(dir / "test.js.map")
+    assert(testJsSize > 0)
+    assert(testJsMapSize > 0)
 
-  //   val runRes = os.proc("node", "test.js").call(cwd = dir)
-  //   val runOutput = runRes.out.trim()
-  //   assert(runOutput == "asdf 2")
+    val runRes = os.proc("node", "test.js").call(cwd = dir)
+    val runOutput = runRes.out.trim()
+    assert(runOutput == "asdf 2")
 
-  //   os.makeDir.all(dir / "test-output")
-  //   os.proc(
-  //     launcher,
-  //     "--stdlib",
-  //     scalaJsLibraryCp,
-  //     "-s",
-  //     "--outputDir",
-  //     "test-output",
-  //     "--moduleSplitStyle",
-  //     "SmallestModules",
-  //     "--moduleKind",
-  //     "CommonJSModule",
-  //     "-mm",
-  //     "Foo.main",
-  //     "bin"
-  //   ).call(cwd = dir, stdin = os.Inherit, stdout = os.Inherit)
+    os.makeDir.all(dir / "test-output")
+    os.proc(
+      launcher,
+      "--stdlib",
+      scalaJsLibraryCp,
+      "-s",
+      "--outputDir",
+      "test-output",
+      "--moduleSplitStyle",
+      "SmallestModules",
+      "--moduleKind",
+      "CommonJSModule",
+      "-mm",
+      "Foo.main",
+      "bin"
+    ).call(cwd = dir, stdin = os.Inherit, stdout = os.Inherit)
 
-  //   val jsFileCount = os.list(dir / "test-output").count { p =>
-  //     p.last.endsWith(".js") && os.isFile(p)
-  //   }
-  //   assert(jsFileCount > 1)
+    val jsFileCount = os.list(dir / "test-output").count { p =>
+      p.last.endsWith(".js") && os.isFile(p)
+    }
+    assert(jsFileCount > 1)
 
-  //   val splitRunRes = os
-  //     .proc("node", "test-output/main.js")
-  //     .call(cwd = dir)
-  //   val splitRunOutput = splitRunRes.out.trim()
-  //   assert(splitRunOutput == "asdf 2")
-  // }
+    val splitRunRes = os
+      .proc("node", "test-output/main.js")
+      .call(cwd = dir)
+    val splitRunOutput = splitRunRes.out.trim()
+    assert(splitRunOutput == "asdf 2")
+  }
 
-  // test("fullLinkJs mode does not throw") {
-  //   val dir = os.temp.dir()
-  //   os.write(
-  //     dir / "foo.scala",
-  //     """object Foo {
-  //       |  def main(args: Array[String]): Unit = {
-  //       |    val s = "Hello"
-  //       |    println("Hello" + s.charAt(5))
-  //       |  }
-  //       |
-  //       |  class A
-  //       |}
-  //       |""".stripMargin
-  //   )
+  test("fullLinkJs mode does not throw") {
+    val dir = os.temp.dir()
+    os.write(
+      dir / "foo.scala",
+      """object Foo {
+        |  def main(args: Array[String]): Unit = {
+        |    val s = "Hello"
+        |    println("Hello" + s.charAt(5))
+        |  }
+        |
+        |  class A
+        |}
+        |""".stripMargin
+    )
 
-  //   val scalaJsLibraryCp = getScalaJsLibraryCp(dir)
+    val scalaJsLibraryCp = getScalaJsLibraryCp(dir)
 
-  //   os.makeDir.all(dir / "bin")
-  //   os.proc(
-  //     "cs",
-  //     "launch",
-  //     "scalac:2.13.12",
-  //     "--",
-  //     "-classpath",
-  //     scalaJsLibraryCp,
-  //     s"-Xplugin:${getScalaJsCompilerPlugin(dir)}",
-  //     "-d",
-  //     "bin",
-  //     "foo.scala"
-  //   ).call(cwd = dir, stdin = os.Inherit, stdout = os.Inherit)
+    os.makeDir.all(dir / "bin")
+    os.proc(
+      "cs",
+      "launch",
+      "scalac:2.13.12",
+      "--",
+      "-classpath",
+      scalaJsLibraryCp,
+      s"-Xplugin:${getScalaJsCompilerPlugin(dir)}",
+      "-d",
+      "bin",
+      "foo.scala"
+    ).call(cwd = dir, stdin = os.Inherit, stdout = os.Inherit)
 
-  //   val res = os
-  //     .proc(
-  //       launcher,
-  //       "--stdlib",
-  //       scalaJsLibraryCp,
-  //       "-s",
-  //       "--fullOpt",
-  //       "-o",
-  //       "test.js",
-  //       "-mm",
-  //       "Foo.main",
-  //       "bin"
-  //     )
-  //     .call(cwd = dir, mergeErrIntoOut = true)
+    val res = os
+      .proc(
+        launcher,
+        "--stdlib",
+        scalaJsLibraryCp,
+        "-s",
+        "--fullOpt",
+        "-o",
+        "test.js",
+        "-mm",
+        "Foo.main",
+        "bin"
+      )
+      .call(cwd = dir, mergeErrIntoOut = true)
 
-  //   val testJsSize = os.size(dir / "test.js")
-  //   val testJsMapSize = os.size(dir / "test.js.map")
-  //   assert(testJsSize > 0)
-  //   assert(testJsMapSize > 0)
+    val testJsSize = os.size(dir / "test.js")
+    val testJsMapSize = os.size(dir / "test.js.map")
+    assert(testJsSize > 0)
+    assert(testJsMapSize > 0)
 
-  //   os.proc("node", "test.js").call(cwd = dir, check = true)
-  // }
+    os.proc("node", "test.js").call(cwd = dir, check = true)
+  }
 
-  // test("fastLinkJs mode throws") {
-  //   val dir = os.temp.dir()
-  //   os.write(
-  //     dir / "foo.scala",
-  //     """object Foo {
-  //       |  def main(args: Array[String]): Unit = {
-  //       |    val s = "Hello"
-  //       |    println("Hello" + s.charAt(5))
-  //       |  }
-  //       |
-  //       |  class A
-  //       |}
-  //       |""".stripMargin
-  //   )
+  test("fastLinkJs mode throws") {
+    val dir = os.temp.dir()
+    os.write(
+      dir / "foo.scala",
+      """object Foo {
+        |  def main(args: Array[String]): Unit = {
+        |    val s = "Hello"
+        |    println("Hello" + s.charAt(5))
+        |  }
+        |
+        |  class A
+        |}
+        |""".stripMargin
+    )
 
-  //   val scalaJsLibraryCp = getScalaJsLibraryCp(dir)
+    val scalaJsLibraryCp = getScalaJsLibraryCp(dir)
 
-  //   os.makeDir.all(dir / "bin")
-  //   os.proc(
-  //     "cs",
-  //     "launch",
-  //     "scalac:2.13.12",
-  //     "--",
-  //     "-classpath",
-  //     scalaJsLibraryCp,
-  //     s"-Xplugin:${getScalaJsCompilerPlugin(dir)}",
-  //     "-d",
-  //     "bin",
-  //     "foo.scala"
-  //   ).call(cwd = dir, stdin = os.Inherit, stdout = os.Inherit)
+    os.makeDir.all(dir / "bin")
+    os.proc(
+      "cs",
+      "launch",
+      "scalac:2.13.12",
+      "--",
+      "-classpath",
+      scalaJsLibraryCp,
+      s"-Xplugin:${getScalaJsCompilerPlugin(dir)}",
+      "-d",
+      "bin",
+      "foo.scala"
+    ).call(cwd = dir, stdin = os.Inherit, stdout = os.Inherit)
 
-  //   os
-  //     .proc(
-  //       launcher,
-  //       "--stdlib",
-  //       scalaJsLibraryCp,
-  //       "--fastOpt",
-  //       "-s",
-  //       "-o",
-  //       "test.js",
-  //       "-mm",
-  //       "Foo.main",
-  //       "bin"
-  //     )
-  //     .call(cwd = dir, mergeErrIntoOut = true)
+    os
+      .proc(
+        launcher,
+        "--stdlib",
+        scalaJsLibraryCp,
+        "--fastOpt",
+        "-s",
+        "-o",
+        "test.js",
+        "-mm",
+        "Foo.main",
+        "bin"
+      )
+      .call(cwd = dir, mergeErrIntoOut = true)
 
-  //   val testJsSize = os.size(dir / "test.js")
-  //   val testJsMapSize = os.size(dir / "test.js.map")
-  //   assert(testJsSize > 0)
-  //   assert(testJsMapSize > 0)
+    val testJsSize = os.size(dir / "test.js")
+    val testJsMapSize = os.size(dir / "test.js.map")
+    assert(testJsSize > 0)
+    assert(testJsMapSize > 0)
 
-  //   val runRes = os.proc("node", "test.js").call(cwd = dir, check = false, stderr = os.Pipe)
-  //   assert(runRes.exitCode == 1)
+    val runRes = os.proc("node", "test.js").call(cwd = dir, check = false, stderr = os.Pipe)
+    assert(runRes.exitCode == 1)
 
-  //   assert(runRes.err.trim().contains("UndefinedBehaviorError"))
-  // }
+    assert(runRes.err.trim().contains("UndefinedBehaviorError"))
+  }
 
   test("import map") {
     val dir = os.temp.dir()
