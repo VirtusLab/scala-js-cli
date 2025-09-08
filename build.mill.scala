6b176b4b0d2c70ed81504b4b1e58a585d9ff67dd
Piotr Chabelski <ged.subfan@gmail.com>
2025-09-08 11:52:56 +0200

    Bump `jsoniter-scala` to 2.37.10

--- build.mill.scala
+++ build.mill.scala
@@ -23,7 +23,7 @@ import com.lumidion.sonatype.central.client.core.{PublishingType, SonatypeCreden
 object Versions {
   def scala213                = "2.13.16"
   def scalaJsVersion          = "1.19.0"
-  def jsoniterVersion         = "2.36.7"
+  def jsoniterVersion         = "2.37.10"
   def scalaJsImportMapVersion = "0.1.1"
   def graalVmVersion          = "22.3.1"
   def munitVersion            = "1.1.1"
